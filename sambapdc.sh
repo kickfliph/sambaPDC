92943a44b81b3a904ceac5dd29739f920d8706ba
Harold Luzardo <haroldburtonl@gmail.com>
2020-06-30 23:39:43 -0400

    working with some authentication problems

--- sambapdc.sh
+++ sambapdc.sh
@@ -12,13 +12,13 @@ if grep -qs "ubuntu" /etc/os-release; then
 	os_version=$(grep 'version_id' /etc/os-release | cut -d '"' -f 2 | tr -d '.')
         apt-get update
         apt-get upgrade -y
-	apt-get install acl attr samba samba-dsdb-modules samba-vfs-modules winbind libpam-winbind libnss-winbind libpam-krb5 krb5-config krb5-user ntp -y
+	apt-get install acl attr samba samba-dsdb-modules samba-vfs-modules winbind libpam-winbind libnss-winbind libpam-krb5 krb5-config krb5-user ntp smbclient -y
 elif [[ -e /etc/debian_version ]]; then
 	os="debian"
 	os_version=$(grep -oe '[0-9]+' /etc/debian_version | head -1)
         apt-get update
         apt-get upgrade -y
-	apt-get install acl attr samba samba-dsdb-modules samba-vfs-modules winbind libpam-winbind libnss-winbind libpam-krb5 krb5-config krb5-user ntp -y
+	apt-get install acl attr samba samba-dsdb-modules samba-vfs-modules winbind libpam-winbind libnss-winbind libpam-krb5 krb5-config krb5-user ntp smbclient -y
 elif [[ -e /etc/centos-release ]]; then
 	os="centos"
 	os_version=$(grep -oe '[0-9]+' /etc/centos-release | head -1)
@@ -37,7 +37,7 @@ Supported distributions are Ubuntu, Debian, CentOS, and Fedora."
 	exit
 fi
 
-samba-tool domain provision --server-role=dc --use-rfc2307 --dns-backend=SAMBA_INTERNAL --realm=samba.example.com --domain=samba --adminpass=1234567890!
+samba-tool domain provision --server-role=dc --use-rfc2307 --dns-backend=SAMBA_INTERNAL --realm=samba.example.com --domain=samba --adminpass=K231010Hkd$
 
 echo search samba.example.com  >> /etc/resolv.conf 
 echo nameserver 10.0.2.15  >> /etc/resolv.conf
