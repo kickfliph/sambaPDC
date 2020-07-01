d2b293756c0a07363b6e38557b43ff35fe226ecb
Harold Luzardo <haroldburtonl@gmail.com>
2020-06-30 23:13:41 -0400

    fixed the -y on the installation process for deb

--- sambapdc.sh
+++ sambapdc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo 10.99.0.1     DC1.samba.example.com     DC1 >> /etc/hosts
+echo "10.99.0.1     DC1.samba.example.com     DC1" >> /etc/hosts
 smbd -b | grep "CONFIGFILE"
 smbd -b | egrep "LOCKDIR|STATEDIR|CACHEDIR|PRIVATE_DIR"
 rm /etc/krb5.conf
@@ -10,15 +10,15 @@ rm /etc/krb5.conf
 if grep -qs "ubuntu" /etc/os-release; then
 	os="ubuntu"
 	os_version=$(grep 'version_id' /etc/os-release | cut -d '"' -f 2 | tr -d '.')
-    apt-get update
-    apt-get upgrade -y
-	apt-get install acl attr samba samba-dsdb-modules samba-vfs-modules winbind libpam-winbind libnss-winbind libpam-krb5 krb5-config krb5-user ntp
+        apt-get update
+        apt-get upgrade -y
+	apt-get install acl attr samba samba-dsdb-modules samba-vfs-modules winbind libpam-winbind libnss-winbind libpam-krb5 krb5-config krb5-user ntp -y
 elif [[ -e /etc/debian_version ]]; then
 	os="debian"
 	os_version=$(grep -oe '[0-9]+' /etc/debian_version | head -1)
-    apt-get update
-    apt-get upgrade -y
-	apt-get install acl attr samba samba-dsdb-modules samba-vfs-modules winbind libpam-winbind libnss-winbind libpam-krb5 krb5-config krb5-user ntp
+        apt-get update
+        apt-get upgrade -y
+	apt-get install acl attr samba samba-dsdb-modules samba-vfs-modules winbind libpam-winbind libnss-winbind libpam-krb5 krb5-config krb5-user ntp -y
 elif [[ -e /etc/centos-release ]]; then
 	os="centos"
 	os_version=$(grep -oe '[0-9]+' /etc/centos-release | head -1)
@@ -42,7 +42,7 @@ smbpasswd -a hluzardo
 smbpasswd -e hluzardo
 echo search samba.example.com  >> /etc/resolv.conf 
 echo nameserver 10.99.0.1  >> /etc/resolv.conf
-samba-tool dns zonecreate <DC1.samba.example.com> 0.99.10.in-addr.arpa
+samba-tool dns zonecreate DC1.samba.example.com 0.99.10.in-addr.arpa
 cp /usr/local/samba/private/krb5.conf /etc/krb5.conf
 systemctl start samba
 smbclient -L localhost -U%
